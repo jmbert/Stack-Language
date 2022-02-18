edef258c8037a47df1aeb16cef6787ef6004faba
jmbert <64034169+jmbert@users.noreply.github.com>
2022-02-18 23:39:08 +0000

    Added concats and string type

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"math"
 	"os"
@@ -27,8 +28,7 @@ func main() {
 		var cmd = strings.Split(lines[i], " ")
 
 		for i := 0; i < len(cmd); i++ {
-
-			var stackCommands = GenerateCommands(cmd[i])
+			var stackCommands = GenerateCommands(cmd[i], s)
 
 			splitcmd := strings.Split(stackCommands, ">")
 			if splitcmd[0] == "Push" {
@@ -85,44 +85,95 @@ func main() {
 				for scanner.Scan() {
 					s.Push(scanner.Text())
 				}
+			} else if splitcmd[0] == "input" {
+				var input string
+				fmt.Scanln(&input)
+				s.Push(input)
 			} else if splitcmd[0] == "Print" {
 				topCmd := s.Pop()
 				println(topCmd)
 				s.Push(topCmd)
+			} else if splitcmd[0] == "PrintStr" {
+				topCmd := s.Pop()
+				var seperated = strings.Split(topCmd, ":")
+				println(seperated[1])
+				s.Push(topCmd)
+			} else if splitcmd[0] == "concat" {
+				firstString := s.Pop()
+				secondString := s.Pop()
+
+				properString1, isString1 := CheckString(firstString)
+				properString2, isString2 := CheckString(secondString)
+
+				properString1 = strings.Replace(properString1, "\"", "", -1)
+				properString2 = strings.Replace(properString2, "\"", "", -1)
+
+				if isString1 == false || isString2 == false {
+					log.Fatal("Invalid Concat")
+				} else {
+					s.Push("string:" + properString2 + properString1)
+				}
 			}
 		}
 	}
 }
 
-func GenerateCommands(command string) string {
-	_, error := strconv.ParseInt(command, 10, 64)
+func GenerateCommands(command string, s Stack) string {
+	if command[0] != '"' {
+		_, error := strconv.ParseInt(command, 10, 64)
 
-	if error != nil {
-		if command == "+" {
-			return "Add"
-		} else if command == "-" {
-			return "Subtract"
-		} else if command == "*" {
-			return "Multiply"
-		} else if command == "/" {
-			return "Divide"
-		} else if command == "PRINT" {
-			return "Print"
-		} else if command == "^" {
-			return "Exponentiate"
-		} else if command == "STDIN" {
-			return "stdin"
-		} else if command == "POP" {
-			return "Pop"
-		} else if command == "PUSH" {
-			return "PushCurrent"
-		} else if command == "DUPE" {
-			return "dupe"
+		if error != nil {
+			if command == "+" {
+				return "Add"
+			} else if command == "-" {
+				return "Subtract"
+			} else if command == "*" {
+				return "Multiply"
+			} else if command == "/" {
+				return "Divide"
+			} else if command == "PRINT" {
+				var top = s.Pop()
+				s.Push(top)
+				var seperated = strings.Split(top, ":")
+				if len(seperated) != 1 {
+					return "PrintStr>" + seperated[1]
+				} else {
+					return "Print"
+				}
+			} else if command == "^" {
+				return "Exponentiate"
+			} else if command == "STDIN" {
+				return "stdin"
+			} else if command == "POP" {
+				return "Pop"
+			} else if command == "PUSH" {
+				return "PushCurrent"
+			} else if command == "DUPE" {
+				return "dupe"
+			} else if command == "INPUT" {
+				return "input"
+			} else if command == "CONCAT" {
+				return "concat"
+			} else {
+				return "Push>" + command
+			}
 		} else {
 			return "Push>" + command
 		}
 	} else {
-		return "Push>" + command
+
+		return "Push>" + ("string:" + strings.Split(command, "|")[0])
+	}
+
+}
+
+func CheckString(command string) (string, bool) {
+	var seperated = strings.Split(command, ":")
+
+	if len(seperated) != 1 {
+		return seperated[1], true
+	} else {
+		return "", false
 	}
 }
 
